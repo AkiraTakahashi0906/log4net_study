e35382db3721aa50f56669cec7cd4588495f0da9
akira takahashi <akira.takahashi.s2000@gmail.com>
2020-07-09 21:17:52 +0900

    プロジェクト ファイルを追加します。

---
+++ log4net_study/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace log4net_study
+{
+    static class Program
+    {
+        //log4net
+        private static log4net.ILog _logger = log4net.LogManager
+                                            .GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            _logger.Debug("デバッグ");
+            _logger.Info("インフォ");
+            _logger.Warn("警告");
+            _logger.Error("エラー");
+            _logger.Fatal("致命的");
+
+            Application.Run(new Form1());
+        }
+    }
+}
