6699a435f360dcf85503c6d2400799badfa82fbe
akira takahashi <akira.takahashi.s2000@gmail.com>
2020-09-12 10:28:22 +0900

    log errer

--- log4net_study/Form1.cs
+++ log4net_study/Form1.cs
@@ -12,9 +12,28 @@ namespace log4net_study
 {
     public partial class Form1 : Form
     {
+        //log4net
+        private static log4net.ILog _logger = log4net.LogManager
+                                            .GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public Form1()
         {
             InitializeComponent();
+            _logger.Info("InitializeComponent");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var array = new int[5];
+
+            try
+            {
+                array[6] = 1;
+            }
+            catch(Exception ex)
+            {
+                _logger.Error(ex.ToString());
+            }
         }
     }
 }
